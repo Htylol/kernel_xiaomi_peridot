vaesumed/linux-xlnx

USB: remove unnecessary type casting of urb->context
urb->context code cleanup

Signed-off-by: Ming Lei <tom.leiming@gmail.com>
Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

drivers/usb/class/cdc-acm.c
@@ -450,7 +450,7 @@ urbs:
 /* data interface wrote those outgoing bytes */
 static void acm_write_bulk(struct urb *urb)
 {
-	struct acm *acm = (struct acm *)urb->context;
+	struct acm *acm = urb->context;
 
 	dbg("Entering acm_write_bulk with status %d", urb->status);
 

Htylol/kernel_xiaomi_peridot

drivers/usb/class/cdc-acm.c
@@ -443,7 +443,7 @@ urbs:
 static void acm_write_bulk(struct urb *urb)
 {
 	struct acm *acm;
-	struct acm_wb *wb = (struct acm_wb *)urb->context;
+	struct acm_wb *wb = urb->context;
 
 	dbg("Entering acm_write_bulk with status %d", urb->status);
 