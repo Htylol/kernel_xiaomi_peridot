bzy-080408/android_pkvm_kernel

ANDROID: KVM: arm64: Remove 'pending reclaim' page flags
The HOST_PAGE_PENDING_RECLAIM and HOST_PAGE_NEED_POISONING flags are now
no longer used, remove them.

Bug: 229972313
Bug: 238945523
Change-Id: Ib88a22c486f9c5eab95f0763ff949426f883be83
Signed-off-by: Quentin Perret <qperret@google.com>

arch/arm64/kvm/hyp/include/nvhe/memory.h
@@ -11,9 +11,7 @@
  * Accesses to struct hyp_page flags are serialized by the host stage-2
  * page-table lock.
  */
-#define HOST_PAGE_NEED_POISONING	BIT(0)
-#define HOST_PAGE_PENDING_RECLAIM	BIT(1)
-#define HOST_PAGE_WRPROTECT		BIT(2)
+#define HOST_PAGE_WRPROTECT		BIT(0)
 
 struct hyp_page {
 	unsigned short refcount;

Htylol/kernel_xiaomi_peridot

arch/arm64/kvm/hyp/include/nvhe/memory.h
@@ -11,9 +11,7 @@
  * Accesses to struct hyp_page flags are serialized by the host stage-2
  * page-table lock.
  */
-#define HOST_PAGE_NEED_POISONING	BIT(0)
-#define HOST_PAGE_PENDING_RECLAIM	BIT(1)
-#define MODULE_OWNED_PAGE		BIT(2)
+#define MODULE_OWNED_PAGE		BIT(0)
 
 struct hyp_page {
 	unsigned short refcount;