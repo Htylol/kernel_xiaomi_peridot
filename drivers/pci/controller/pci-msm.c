MotorolaMobilityLLC/kernel-msm

msm: pcie: Remove usage of pr_alert()
pr_alert() is used in this driver for cases which do not seem to
fall under alert category. In addition, there is a side effect
of introducing console latencies when messages with alert
severity get sent to the console.

Fix pr_alert() usage by using other pr_* macros appropriately.

Change-Id: I5dffd744e3b5977f8826c669e2948e9de160727d
Signed-off-by: Faisal Hassan <quic_faisalh@quicinc.com>

drivers/pci/controller/pci-msm.c
@@ -8347,7 +8347,7 @@ static int __init pcie_init(void)
 	int ret = 0, i;
 	char rc_name[MAX_RC_NAME_LEN];
 
-	pr_alert("pcie:%s.\n", __func__);
+	pr_info("pcie:%s.\n", __func__);
 
 	pcie_drv.rc_num = 0;
 	mutex_init(&pcie_drv.drv_lock);

Htylol/kernel_xiaomi_peridot

Signed-off-by: Prudhvi Yarlagadda <quic_pyarlaga@quicinc.com>

drivers/pci/controller/pci-msm.c
@@ -8745,7 +8745,7 @@ static int __init pcie_init(void)
 	char rc_name[MAX_RC_NAME_LEN];
 	void __iomem *reg_addr;
 
-	pr_alert("pcie:%s.\n", __func__);
+	pr_debug("pcie:%s.\n", __func__);
 
 	pcie_drv.rc_num = 0;
 	mutex_init(&pcie_drv.drv_lock);