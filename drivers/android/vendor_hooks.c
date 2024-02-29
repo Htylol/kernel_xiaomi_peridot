BayLibre/android_kernel_common

ANDROID: Add vendor_hooks to workaround CONFIG_TASK_DELAY_ACCT
CONFIG_TASK_DELAY_ACCT cannot be enabled since `struct task_struct`
is KMI frozen. Instead, use vendor hooks to allow delay accounting
to be implemented in a vendor module.

Bug: 310129610
Bug: 314931189

Change-Id: If814d7834889fe162aba3dd97e935289127ca3ae
Signed-off-by: Dongyun Liu <dongyun.liu@transsion.com>

drivers/android/vendor_hooks.c
@@ -110,6 +110,7 @@
 #include <trace/hooks/direct_io.h>
 #include <trace/hooks/loop.h>
 #include <trace/hooks/psi.h>
+#include <trace/hooks/delayacct.h>
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
  * associated with them) to allow external modules to probe them.
@@ -467,6 +468,20 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_transaction_received);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_free_oem_binder_struct);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_special_task);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_free_buf);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_delayacct_set_flag);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_delayacct_clear_flag);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_delayacct_init);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_delayacct_tsk_init);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_delayacct_tsk_free);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_delayacct_blkio_start);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_delayacct_blkio_end);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_delayacct_add_tsk);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_delayacct_blkio_ticks);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_delayacct_is_task_waiting_on_io);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_delayacct_freepages_start);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_delayacct_freepages_end);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_delayacct_thrashing_start);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_delayacct_thrashing_end);
 /*
  * For type visibility
  */

include/trace/hooks/delayacct.h
@@ -1,0 +1,67 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifdef PROTECT_TRACE_INCLUDE_PATH
+#undef PROTECT_TRACE_INCLUDE_PATH
+
+
+#else /* PROTECT_TRACE_INCLUDE_PATH */
+
+#undef TRACE_SYSTEM
+#define TRACE_SYSTEM delayacct
+
+#define TRACE_INCLUDE_PATH trace/hooks
+
+#if !defined(_TRACE_HOOK_DELAYACCT_H) || defined(TRACE_HEADER_MULTI_READ)
+#define _TRACE_HOOK_DELAYACCT_H
+
+#include <trace/hooks/vendor_hooks.h>
+
+struct task_struct;
+struct taskstats;
+DECLARE_HOOK(android_vh_delayacct_set_flag,
+	TP_PROTO(struct task_struct *p, int flag),
+	TP_ARGS(p, flag));
+DECLARE_HOOK(android_vh_delayacct_clear_flag,
+	TP_PROTO(struct task_struct *p, int flag),
+	TP_ARGS(p, flag));
+DECLARE_RESTRICTED_HOOK(android_rvh_delayacct_init,
+	TP_PROTO(void *unused),
+	TP_ARGS(unused), 1);
+DECLARE_RESTRICTED_HOOK(android_rvh_delayacct_tsk_init,
+	TP_PROTO(struct task_struct *tsk),
+	TP_ARGS(tsk), 1);
+DECLARE_RESTRICTED_HOOK(android_rvh_delayacct_tsk_free,
+	TP_PROTO(struct task_struct *tsk),
+	TP_ARGS(tsk), 1);
+DECLARE_HOOK(android_vh_delayacct_blkio_start,
+	TP_PROTO(void *unused),
+	TP_ARGS(unused));
+DECLARE_HOOK(android_vh_delayacct_blkio_end,
+	TP_PROTO(struct task_struct *p),
+	TP_ARGS(p));
+DECLARE_HOOK(android_vh_delayacct_add_tsk,
+	TP_PROTO(struct taskstats *d, struct task_struct *tsk, int *ret),
+	TP_ARGS(d, tsk, ret));
+DECLARE_HOOK(android_vh_delayacct_blkio_ticks,
+	TP_PROTO(struct task_struct *tsk, __u64 *ret),
+	TP_ARGS(tsk, ret));
+DECLARE_HOOK(android_vh_delayacct_is_task_waiting_on_io,
+	TP_PROTO(struct task_struct *tsk, int *ret),
+	TP_ARGS(tsk, ret));
+DECLARE_HOOK(android_vh_delayacct_freepages_start,
+	TP_PROTO(void *unused),
+	TP_ARGS(unused));
+DECLARE_HOOK(android_vh_delayacct_freepages_end,
+	TP_PROTO(void *unused),
+	TP_ARGS(unused));
+DECLARE_HOOK(android_vh_delayacct_thrashing_start,
+	TP_PROTO(void *unused),
+	TP_ARGS(unused));
+DECLARE_HOOK(android_vh_delayacct_thrashing_end,
+	TP_PROTO(void *unused),
+	TP_ARGS(unused));
+
+#endif /* _TRACE_HOOK_DELAYACCT_H */
+/* This part must be outside protection */
+#include <trace/define_trace.h>
+
+#endif /* PROTECT_TRACE_INCLUDE_PATH */

Htylol/kernel_xiaomi_peridot

Bug: 327566572
(cherry picked from commit bb57557246d39dba8a66df7f43983fe1ec71bff6)
(cherry picked from commit 896cff873452d9a3853c489bb2a173a1e290ca95)

drivers/android/vendor_hooks.c
@@ -70,6 +70,8 @@
 #include <trace/hooks/regmap.h>
 #include <trace/hooks/compaction.h>
 #include <trace/hooks/suspend.h>
+#include <trace/hooks/delayacct.h>
+
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
  * associated with them) to allow external modules to probe them.
@@ -380,3 +382,21 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_looper_exited);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_spawn_new_thread);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_has_special_work_ilocked);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_detect_low_async_space_locked);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_delayacct_init);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_delayacct_tsk_init);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_delayacct_tsk_free);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_delayacct_blkio_start);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_delayacct_blkio_end);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_delayacct_add_tsk);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_delayacct_blkio_ticks);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_delayacct_freepages_start);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_delayacct_freepages_end);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_delayacct_thrashing_start);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_delayacct_thrashing_end);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_delayacct_swapin_start);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_delayacct_swapin_end);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_delayacct_compact_start);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_delayacct_compact_end);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_delayacct_wpcopy_start);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_delayacct_wpcopy_end);
+

include/trace/hooks/delayacct.h
@@ -1,0 +1,76 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifdef PROTECT_TRACE_INCLUDE_PATH
+#undef PROTECT_TRACE_INCLUDE_PATH
+
+
+#else /* PROTECT_TRACE_INCLUDE_PATH */
+
+#undef TRACE_SYSTEM
+#define TRACE_SYSTEM delayacct
+
+#define TRACE_INCLUDE_PATH trace/hooks
+
+#if !defined(_TRACE_HOOK_DELAYACCT_H) || defined(TRACE_HEADER_MULTI_READ)
+#define _TRACE_HOOK_DELAYACCT_H
+
+#include <trace/hooks/vendor_hooks.h>
+
+struct task_struct;
+struct taskstats;
+DECLARE_RESTRICTED_HOOK(android_rvh_delayacct_init,
+	TP_PROTO(void *unused),
+	TP_ARGS(unused), 1);
+DECLARE_RESTRICTED_HOOK(android_rvh_delayacct_tsk_init,
+	TP_PROTO(struct task_struct *tsk),
+	TP_ARGS(tsk), 1);
+DECLARE_RESTRICTED_HOOK(android_rvh_delayacct_tsk_free,
+	TP_PROTO(struct task_struct *tsk),
+	TP_ARGS(tsk), 1);
+DECLARE_HOOK(android_vh_delayacct_blkio_start,
+	TP_PROTO(void *unused),
+	TP_ARGS(unused));
+DECLARE_HOOK(android_vh_delayacct_blkio_end,
+	TP_PROTO(struct task_struct *p),
+	TP_ARGS(p));
+DECLARE_HOOK(android_vh_delayacct_add_tsk,
+	TP_PROTO(struct taskstats *d, struct task_struct *tsk, int *ret),
+	TP_ARGS(d, tsk, ret));
+DECLARE_HOOK(android_vh_delayacct_blkio_ticks,
+	TP_PROTO(struct task_struct *tsk, __u64 *ret),
+	TP_ARGS(tsk, ret));
+DECLARE_HOOK(android_vh_delayacct_freepages_start,
+	TP_PROTO(void *unused),
+	TP_ARGS(unused));
+DECLARE_HOOK(android_vh_delayacct_freepages_end,
+	TP_PROTO(void *unused),
+	TP_ARGS(unused));
+DECLARE_HOOK(android_vh_delayacct_thrashing_start,
+	TP_PROTO(bool *in_thrashing),
+	TP_ARGS(in_thrashing));
+DECLARE_HOOK(android_vh_delayacct_thrashing_end,
+	TP_PROTO(bool *in_thrashing),
+	TP_ARGS(in_thrashing));
+DECLARE_HOOK(android_vh_delayacct_swapin_start,
+	TP_PROTO(void *unused),
+	TP_ARGS(unused));
+DECLARE_HOOK(android_vh_delayacct_swapin_end,
+	TP_PROTO(void *unused),
+	TP_ARGS(unused));
+DECLARE_HOOK(android_vh_delayacct_compact_start,
+	TP_PROTO(void *unused),
+	TP_ARGS(unused));
+DECLARE_HOOK(android_vh_delayacct_compact_end,
+	TP_PROTO(void *unused),
+	TP_ARGS(unused));
+DECLARE_HOOK(android_vh_delayacct_wpcopy_start,
+	TP_PROTO(void *unused),
+	TP_ARGS(unused));
+DECLARE_HOOK(android_vh_delayacct_wpcopy_end,
+	TP_PROTO(void *unused),
+	TP_ARGS(unused));
+
+#endif /* _TRACE_HOOK_DELAYACCT_H */
+/* This part must be outside protection */
+#include <trace/define_trace.h>
+
+#endif /* PROTECT_TRACE_INCLUDE_PATH */